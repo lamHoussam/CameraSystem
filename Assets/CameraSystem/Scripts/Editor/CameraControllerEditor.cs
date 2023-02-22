255e6effaa1d49fb58a2150873d244944f21f6cb
houssam lamlih <houssam.lamlih@hotmail.com>
2023-02-22 14:18:34 +0100

    Camera Controller Editor

--- Assets/CameraSystem/Scripts/Editor/CameraControllerEditor.cs
+++ Assets/CameraSystem/Scripts/Editor/CameraControllerEditor.cs
@@ -1,11 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
-
-[CustomEditor(typeof(CameraController))]
-public class CameraControllerEditor : Editor
+namespace CameraSystem
 {
-    
+    [CustomEditor(typeof(CameraController))]
+    public class CameraControllerEditor : Editor
+    {
+        private SerializedProperty spTarget;
+        private SerializedProperty spDistance;
+        private SerializedProperty spOffset;
+        private SerializedProperty spCameraLerpTime;
+
+
+        // Yaw Pitch extremes
+        private SerializedProperty spMinPitchValue, spMaxPitchValue;
+        private SerializedProperty spMinYawValue, spMaxYawValue;
+
+        // Collision
+        private SerializedProperty spEnableCameraCollision;
+        private SerializedProperty spCameraCollisionLayer;
+
+        private void OnEnable()
+        {
+            spTarget = serializedObject.FindProperty("m_Target");
+            spDistance = serializedObject.FindProperty("m_distance");
+            spOffset = serializedObject.FindProperty("m_offset");
+            spCameraLerpTime = serializedObject.FindProperty("m_cameraLerpTime");
+
+            spMinPitchValue = serializedObject.FindProperty("m_minPitchValue");
+            spMaxPitchValue = serializedObject.FindProperty("m_maxPitchValue");
+
+            spMinYawValue = serializedObject.FindProperty("m_yawMinValue");
+            spMaxYawValue = serializedObject.FindProperty("m_yawMaxValue");
+
+            spEnableCameraCollision = serializedObject.FindProperty("m_enableCameraCollision");
+            spCameraCollisionLayer = serializedObject.FindProperty("m_cameraCollisionLayer");
+
+            spEnableCameraCollision = serializedObject.FindProperty("m_enableCameraCollision");
+            spCameraCollisionLayer = serializedObject.FindProperty("m_cameraCollisionLayer");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(spTarget);
+
+
+            // Values
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUILayout.LabelField("Values", EditorStyles.boldLabel);
+
+            using (new EditorGUI.IndentLevelScope())
+            {
+                EditorGUILayout.PropertyField(spDistance);
+                EditorGUILayout.PropertyField(spOffset);
+                EditorGUILayout.PropertyField(spCameraLerpTime);
+            }
+
+            EditorGUILayout.EndVertical();
+
+
+            // Angles
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUILayout.LabelField("Angles", EditorStyles.boldLabel);
+
+            using (new EditorGUI.IndentLevelScope())
+            {
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(spMinYawValue);
+                EditorGUILayout.PropertyField(spMaxYawValue);
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PropertyField(spMinPitchValue);
+                EditorGUILayout.PropertyField(spMaxPitchValue);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.EndVertical();
+
+
+            // Collisions
+            EditorGUILayout.Space();
+            EditorGUILayout.PropertyField(spEnableCameraCollision);
+            if (spEnableCameraCollision.boolValue)
+            {
+                EditorGUILayout.BeginVertical(GUI.skin.box);
+
+                using (new EditorGUI.IndentLevelScope())
+                {
+                    EditorGUILayout.PropertyField(spCameraCollisionLayer);
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
--- Assets/CameraSystem/Scripts/Source/CameraController.cs
+++ Assets/CameraSystem/Scripts/Source/CameraController.cs
@@ -1,100 +1,103 @@
 using System.Collections;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace CameraSystem
 {
-    [SerializeField] private Transform m_target;
-
-    [SerializeField] private float m_distance;
-    [SerializeField] private Vector2 m_offset;
-
-    [SerializeField] private float m_cameraLerpTime;
-
-    private float m_targetDistance;
-    private Vector3 m_expectedPos, m_realOffset;
-
-    [SerializeField] private float m_minPitchValue = -10, m_maxPitchValue = 70;
-    [SerializeField] private float m_yawMinValue = -50, m_yawMaxValue = 50;
-
-    [SerializeField] private bool m_enableCameraCollision;
-    [SerializeField] private LayerMask m_cameraLayerMask;
-
-
-    private float m_yaw, m_pitch;
-    private void LateUpdate()
+    public class CameraController : MonoBehaviour
     {
-        if (!m_target) return;
-        SetPitchYaw();
-        ThirdPersonCamera();
-    }
+        [SerializeField] private Transform m_Target;
 
-    public void SetPitchYaw()
-    {
-        m_pitch += Input.GetAxis("Mouse Y");
-        m_yaw += Input.GetAxis("Mouse X");
+        [SerializeField] private float m_distance;
+        [SerializeField] private Vector2 m_offset;
 
-        m_pitch = ClampAngle(m_pitch, m_minPitchValue, m_maxPitchValue);
-        m_yaw = ClampAngle(m_yaw, m_yawMinValue, m_yawMaxValue);
+        [SerializeField] private float m_cameraLerpTime;
 
-    }
+        private float m_targetDistance;
+        private Vector3 m_expectedPos, m_realOffset;
 
-    private void ThirdPersonCamera()
-    {
-        m_targetDistance = m_distance;
-        //if (m_Player)
-        //    m_targetDistance = m_Player.Aiming ? m_aimingDistance : m_normalDistance;
-        m_distance = Mathf.Lerp(m_distance, m_targetDistance, Time.deltaTime * 5);
+        [SerializeField] private float m_minPitchValue = -10, m_maxPitchValue = 70;
+        [SerializeField] private float m_yawMinValue = -50, m_yawMaxValue = 50;
 
-        float hor = -m_distance * Mathf.Cos(m_pitch * Mathf.Deg2Rad);
+        [SerializeField] private bool m_enableCameraCollision;
+        [SerializeField] private LayerMask m_cameraCollisionLayer;
 
 
-        Vector3 t = (m_expectedPos - m_target.position);
-        Vector3 dir = RotateVector(t, 90).normalized;
+        private float m_yaw, m_pitch;
+        private void LateUpdate()
+        {
+            if (!m_Target) return;
+            SetPitchYaw();
+            ThirdPersonCamera();
+        }
 
-        m_realOffset = dir * m_offset.x + Vector3.up * m_offset.y;
+        public void SetPitchYaw()
+        {
+            m_pitch += Input.GetAxis("Mouse Y");
+            m_yaw += Input.GetAxis("Mouse X");
 
-        Vector3 pos = m_target.position + new Vector3(
-            hor * Mathf.Sin(m_yaw * Mathf.Deg2Rad),
-            m_distance * Mathf.Sin(m_pitch * Mathf.Deg2Rad),
-            hor * Mathf.Cos(m_yaw * Mathf.Deg2Rad)
-        );
-        Quaternion rot = Quaternion.Euler(/*(Vector3)m_offset + */Vector3.up * (m_yaw) + Vector3.right * m_pitch);
+            m_pitch = ClampAngle(m_pitch, m_minPitchValue, m_maxPitchValue);
+            m_yaw = ClampAngle(m_yaw, m_yawMinValue, m_yawMaxValue);
 
-        m_expectedPos = pos;
+        }
 
-        Vector3 direction = (pos - m_target.position).normalized;
+        private void ThirdPersonCamera()
+        {
+            m_targetDistance = m_distance;
+            //if (m_Player)
+            //    m_targetDistance = m_Player.Aiming ? m_aimingDistance : m_normalDistance;
+            m_distance = Mathf.Lerp(m_distance, m_targetDistance, Time.deltaTime * 5);
 
-        Ray ray = new Ray((m_target.position + m_realOffset), direction);
-
-        Vector3 finalTargetPosition;
+            float hor = -m_distance * Mathf.Cos(m_pitch * Mathf.Deg2Rad);
 
 
-        if (m_enableCameraCollision && Physics.Raycast(ray, out RaycastHit hit, m_distance, m_cameraLayerMask))
-            finalTargetPosition = hit.point - direction * .3f;
-        else
-            finalTargetPosition = pos + m_realOffset;
+            Vector3 t = m_expectedPos - m_Target.position;
+            Vector3 dir = RotateVector(t, 90).normalized;
 
-        transform.SetPositionAndRotation(Vector3.Lerp(transform.position, finalTargetPosition, m_cameraLerpTime * Time.deltaTime),
-            Quaternion.Lerp(transform.rotation, rot, m_cameraLerpTime * Time.deltaTime));
+            m_realOffset = dir * m_offset.x + Vector3.up * m_offset.y;
 
-    }
+            Vector3 pos = m_Target.position + new Vector3(
+                hor * Mathf.Sin(m_yaw * Mathf.Deg2Rad),
+                m_distance * Mathf.Sin(m_pitch * Mathf.Deg2Rad),
+                hor * Mathf.Cos(m_yaw * Mathf.Deg2Rad)
+            );
+            Quaternion rot = Quaternion.Euler(/*(Vector3)m_offset + */Vector3.up * m_yaw + Vector3.right * m_pitch);
 
-    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-    {
-        if (lfAngle < -360f) lfAngle += 360f;
-        if (lfAngle > 360f) lfAngle -= 360f;
-        return Mathf.Clamp(lfAngle, lfMin, lfMax);
-    }
+            m_expectedPos = pos;
 
-    public static Vector3 RotateVector(Vector3 vec, float angle)
-    {
-        float cos = Mathf.Cos(angle * Mathf.Deg2Rad);
-        float sin = Mathf.Sin(angle * Mathf.Deg2Rad);
+            Vector3 direction = (pos - m_Target.position).normalized;
 
-        return new Vector3(
-            vec.x * cos - vec.z * sin,
-            0,
-            vec.x * sin + vec.z * cos
-        );
+            Ray ray = new Ray(m_Target.position + m_realOffset, direction);
+
+            Vector3 finalTargetPosition;
+
+
+            if (m_enableCameraCollision && Physics.Raycast(ray, out RaycastHit hit, m_distance, m_cameraCollisionLayer))
+                finalTargetPosition = hit.point - direction * .3f;
+            else
+                finalTargetPosition = pos + m_realOffset;
+
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, finalTargetPosition, m_cameraLerpTime * Time.deltaTime),
+                Quaternion.Lerp(transform.rotation, rot, m_cameraLerpTime * Time.deltaTime));
+
+        }
+
+        private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+        {
+            if (lfAngle < -360f) lfAngle += 360f;
+            if (lfAngle > 360f) lfAngle -= 360f;
+            return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        }
+
+        public static Vector3 RotateVector(Vector3 vec, float angle)
+        {
+            float cos = Mathf.Cos(angle * Mathf.Deg2Rad);
+            float sin = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+            return new Vector3(
+                vec.x * cos - vec.z * sin,
+                0,
+                vec.x * sin + vec.z * cos
+            );
+        }
     }
 }
